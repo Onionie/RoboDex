ea01f4761d07ff227e0a66b9aa6d58ac6adda7b0
Onionie <gutierrez.onionie@gmail.com>
2021-12-21 14:22:08 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      Heroes: [
+        // {
+        //   name: "Iron Man",
+        //   id: "a1",
+        // },
+        // {
+        //   name: "Thor",
+        //   id: "a2",
+        // },
+        // {
+        //   name: "Ant-Man",
+        //   id: "a3",
+        // },
+        // {
+        //   name: "Captain America",
+        //   id: "a4",
+        // },
+        // {
+        //   name: "Hawkeye",
+        //   id: "a5",
+        // },
+        // {
+        //   name: "Black Widow",
+        //   id: "a6",
+        // },
+        // {
+        //   name: "Spider-Man",
+        //   id: "a7",
+        // },
+        // {
+        //   name: "War Machine",
+        //   id: "a8",
+        // },
+        // {
+        //   name: "Falcon",
+        //   id: "a9",
+        // },
+      ],
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ Heroes: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.Heroes.map((hero) => (
+          <h1 key={hero.id}>{hero.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
